527786d9bbc675ff8be39d6ca959b05c7408592a
AYB <151089684+Aboysp2@users.noreply.github.com>
2026-09-16 05:34:56 +0200

    Create urls.py

---
+++ listings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listing_list, name='listing_list'),
+    path('listing/<int:pk>/', views.listing_detail, name='listing_detail'),
+    path('create/', views.create_listing, name='create_listing'),
+]
